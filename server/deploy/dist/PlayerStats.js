58d3981d30e590dc7b12989cdfe48fc56a92e59f
romanzdarek <roman.zdarek@gmail.com>
2023-01-19 23:46:12 +0100

    deleting files

--- server/deploy/dist/PlayerStats.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiUGxheWVyU3RhdHMuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi9zcmMvUGxheWVyU3RhdHMudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7QUFLQyxDQUFDIn0=
